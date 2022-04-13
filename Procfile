c6971bdd6b4714cf35734317e8ab660843ad6191
Louis Roblin <louis@MacBook-Air-de-Louis.local>
2022-04-13 14:14:23 +0200

    commit folder to dev

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python migration.py ; python main.py $PORT
---
+++ helpers.py
@@ -0,0 +1,7 @@
+def addition(a, b):
+    """
+    >>> addition(2, 4)
+    {'result': 6}
+    """
+    y = int(a) + int(b)
+    return {"result": y}
